10bc2276478d615c35a62b487f8bbdaf8c2e339d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-11-06 21:12:53 -0500

    working

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,24 +2,62 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="Cypress" />
 
-context('Example Cypress TodoMVC test', () => {
-  beforeEach(() => {
-    // usually we recommend setting baseUrl in cypress.json
-    // but for simplicity of this example we just use it here
-    // https://on.cypress.io/visit
-    cy.visit('http://todomvc.com/examples/vue/')
-  })
+// it('skips test', function () {
+//   cy.log('skipping')
+//   cy.wrap('foo').should('equal', 'foo')
+//   this.skip()
+// })
 
-  it('adds 2 todos', function () {
-    cy.get('.new-todo')
-      .type('learn testing{enter}')
-      .type('be cool{enter}')
-    cy.get('.todo-list li').should('have.length', 2)
-  })
+// it('finds Mocha context', () => {
+//   cy.log('finding context')
+//     .wait(1000)
+//     .then(() => {
+//       cy.state('runnable').ctx.skip()
+//     })
+// })
 
-  // more examples
-  //
-  // https://github.com/cypress-io/cypress-example-todomvc
-  // https://github.com/cypress-io/cypress-example-kitchensink
-  // https://on.cypress.io/writing-your-first-test
+Cypress.Commands.add('skip', () => {
+  cy.state('runnable').ctx.skip()
 })
+
+it('skip test using custom command', () => {
+  cy.log('about to run custom command to skip this test')
+    .wait(1000)
+    .skip()
+})
+
+// it('puts things into Mocha context', function () {
+//   cy.log('adding a property to ctx')
+//   cy.state('runnable').ctx.foo = 'bar'
+
+//   expect(this.foo, 'context property is now available').to.equal('bar')
+
+//   // cy.wrap('bar')
+//   //   .as('foo')
+//   //   .then(() => {
+//   //     console.log('Mocha context')
+//   //     console.log(cy.state('runnable').ctx)
+//   //   })
+
+//   // but to add it to aliases we need to place into state("aliases") too
+//   const addAlias = (name, value) => {
+//     const aliases = cy.state('aliases') ? cy.state('aliases') : {}
+//     cy.state('runnable').ctx[name] = value
+//     // under aliases, it is kept in an object
+//     aliases[name] = {
+//       alias: name,
+//       subject: value,
+//       command: {
+//         get () {
+//           // cy.get("@alias") checks the command name that saved the alias
+//           // using alias.command.get('name')
+//           // so we return undefined here to duck type it
+//         }
+//       }
+//     }
+//     cy.state('aliases', aliases)
+//   }
+
+//   addAlias('foo', 'bar')
+//   cy.get('@foo').should('equal', 'bar')
+// })
