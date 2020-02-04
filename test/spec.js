8a191fed43eb0f24538f85a1c657058da3ee4b17
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-04 11:37:40 -0500

    test on electron and on chrome

--- test/spec.js
+++ test/spec.js
@@ -5,35 +5,66 @@ const debug = require('debug')('test')
 
 /* eslint-env mocha */
 describe('skipping at run-time', () => {
+  // particular test we want to focus on
+  const title = 'skips on Electron'
+
+  const assertResults = results => {
+    // let's confirm the number of tests
+    // and maybe some additional information
+    debug('results %o', results)
+
+    spok(assert, results, {
+      totalSuites: 0,
+      totalTests: 4,
+      runs: spok.array
+    })
+    assert(results.runs.length === 1, 'single run')
+  }
+
+  const findOurTest = results => {
+    const testSkippedOnElectron = results.runs[0].tests.find(
+      t => t.title[0] === title
+    )
+    debug('found test %o', testSkippedOnElectron)
+    assert(testSkippedOnElectron, 'could not find test')
+    return testSkippedOnElectron
+  }
+
   it('skips test on Electron', () => {
     // by default the test runs on Electron
     return cypress
       .run({
-        spec: 'cypress/integration/spec.js'
+        spec: 'cypress/integration/spec.js',
+        video: false
       })
       .then(results => {
-        // let's confirm the number of tests
-        // and maybe some additional information
-        debug('results %o', results)
-
-        spok(assert, results, {
-          totalSuites: 0,
-          totalTests: 4,
-          runs: spok.array
-        })
-        assert(results.runs.length === 1, 'single run')
-
-        // let's look at the run, and at particular tests
-        const title = 'skips on Electron'
-        const testSkippedOnElectron = results.runs[0].tests.find(
-          t => t.title[0] === title
-        )
-        debug('found test %o', testSkippedOnElectron)
+        assertResults(results)
+        const test = findOurTest(results)
 
         // the test should be pending (which means it was skipped)
-        spok(assert, testSkippedOnElectron, {
+        spok(assert, test, {
           title,
           state: 'pending',
+          error: null
+        })
+      })
+  })
+
+  it('does not skip test in Chrome', () => {
+    return cypress
+      .run({
+        spec: 'cypress/integration/spec.js',
+        browser: 'chrome',
+        video: false
+      })
+      .then(results => {
+        assertResults(results)
+        const test = findOurTest(results)
+
+        // the test should be pending (which means it was skipped)
+        spok(assert, test, {
+          title,
+          state: 'passed',
           error: null
         })
       })
